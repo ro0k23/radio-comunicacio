755342fac2007362a367005585a901f2feed9abd
ro0k23 <125355361+ro0k23@users.noreply.github.com>
2023-03-30 09:35:10 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ radio.onReceivedNumber(function (receivedNumber) {
     basic.showString("" + (receivedNumber))
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("")
+    radio.sendString("fatoudiakhate")
+    basic.showIcon(IconNames.Butterfly)
+    basic.pause(200)
+    basic.clearScreen()
 })
 radio.setGroup(1)
-basic.forever(function () {
-	
-})
